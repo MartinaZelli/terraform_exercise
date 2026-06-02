3ec585261bd78520dcdcd69570f9284b50d7b210
MartinaZelli <martinazelli@live.it>
2026-06-02 18:47:43 +0200

    Fix formatting and remove commented-out disk size in VM definitions for clarity

--- locals.tf
+++ locals.tf
@@ -6,13 +6,21 @@ locals {
       mac      = "02:00:00:01:00:01"
       memory   = 2048 # MiB
       vcpu     = 2
-      disk_gb  = 10   # 20 GB
+      disk_gb  = 10
+    },
+    "app2" = {
+      hostname = "menu-app-2"
+      ip       = "192.168.1.74"
+      mac      = "02:00:00:01:00:03"
+      memory   = 2048
+      vcpu     = 2
+      disk_gb  = 10
     },
     "db" = {
       hostname = "menu-db"
       ip       = "192.168.1.73"
       mac      = "02:00:00:01:00:02"
-      memory   = 2048 
+      memory   = 2048
       vcpu     = 2
       disk_gb  = 10
     }
