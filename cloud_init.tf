137f91a2c716703db823c90c43281a511500a1a0
MartinaZelli <martinazelli@live.it>
2026-06-03 19:39:08 +0200

    Refactor cloud-init and locals configuration for improved clarity and maintainability

--- cloud_init.tf
+++ cloud_init.tf
@@ -1,53 +1,53 @@
 resource "libvirt_cloudinit_disk" "vm_init" {
-   for_each = local.vms
+  for_each = local.vms
   name     = "${each.value.hostname}-cloudinit"
 
-  meta_data = <<EOF
-instance-id: ${each.value.hostname}
-local-hostname: ${each.value.hostname}
-EOF
+  meta_data = <<-EOF
+    instance-id: ${each.value.hostname}
+    local-hostname: ${each.value.hostname}
+  EOF
 
-  user_data = <<EOF
-#cloud-config
-hostname: ${each.value.hostname}.local
-users:
-  - name: ubuntu
-    groups: sudo
-    shell: /bin/bash
-    sudo: ['ALL=(ALL) NOPASSWD:ALL']
-    ssh_authorized_keys:
-      - ${var.ssh_public_key}
-write_files:
-  - path: /etc/apt/preferences.d/no-snapd
-    content: |
-      Package: snapd
-      Pin: release a=*
-      Pin-Priority: -10
-package_update: true
-package_upgrade: false
-packages:
-  - qemu-guest-agent
-runcmd:
-  - DEBIAN_FRONTEND=noninteractive apt-get -y purge unattended-upgrades snapd
-  - DEBIAN_FRONTEND=noninteractive apt-get -y autoremove --purge
-  - rm -rf /root/snap /home/ubuntu/snap
-EOF
+  user_data = <<-EOF
+    #cloud-config
+    hostname: ${each.value.hostname}.local
+    users:
+      - name: ubuntu
+        groups: sudo
+        shell: /bin/bash
+        sudo: ['ALL=(ALL) NOPASSWD:ALL']
+        ssh_authorized_keys:
+          - ${file(var.ssh_public_key_path)}
+    write_files:
+      - path: /etc/apt/preferences.d/no-snapd
+        content: |
+          Package: snapd
+          Pin: release a=*
+          Pin-Priority: -10
+    package_update: true
+    package_upgrade: false
+    packages:
+      - qemu-guest-agent
+    runcmd:
+      - DEBIAN_FRONTEND=noninteractive apt-get -y purge unattended-upgrades snapd
+      - DEBIAN_FRONTEND=noninteractive apt-get -y autoremove --purge
+      - rm -rf /root/snap /home/ubuntu/snap
+  EOF
 
-  network_config = <<EOF
-version: 2
-ethernets:
-  eth0:
-    match:
-      macaddress: "${each.value.mac}"
-    set-name: eth0
-    addresses:
-      - "${each.value.ip}/24"
-    gateway4: "192.168.1.1"
-    nameservers:
-      addresses:
-        - ${var.dns1}
-        - ${var.dns2}
-    dhcp4: false
-    dhcp6: false
-EOF
+  network_config = <<-EOF
+    version: 2
+    ethernets:
+      eth0:
+        match:
+          macaddress: "${each.value.mac}"
+        set-name: eth0
+        addresses:
+          - "${each.value.ip}/24"
+        gateway4: "192.168.1.1"
+        nameservers:
+          addresses:
+            - ${var.dns1}
+            - ${var.dns2}
+        dhcp4: false
+        dhcp6: false
+  EOF
 }
--- locals.tf
+++ locals.tf
@@ -1,36 +1,37 @@
 locals {
-  vms = {
+  # Configurazione standard per evitare ripetizioni
+  vm_default = {
+    memory  = 2048
+    vcpu    = 2
+    disk_gb = 10
+  }
+
+  vms_raw = {
     "app" = {
       hostname = "menu-app"
       ip       = "192.168.1.72"
       mac      = "02:00:00:01:00:01"
-      memory   = 2048 # MiB
-      vcpu     = 2
-      disk_gb  = 10
     },
     "app2" = {
       hostname = "menu-app-2"
       ip       = "192.168.1.74"
       mac      = "02:00:00:01:00:03"
-      memory   = 2048
-      vcpu     = 2
-      disk_gb  = 10
     },
     "db" = {
       hostname = "menu-db"
       ip       = "192.168.1.73"
       mac      = "02:00:00:01:00:02"
-      memory   = 2048
-      vcpu     = 2
-      disk_gb  = 10
     },
     "lb" = {
       hostname = "menu-lb"
-      ip       = "192.168.1.75"          # <-- IP dedicato al Load Balancer
-      mac      = "02:00:00:01:00:04"      # <-- Nuovo MAC Address univoco
-      memory   = 2048
-      vcpu     = 2
-      disk_gb  = 10
+      ip       = "192.168.1.75"
+      mac      = "02:00:00:01:00:04"
+      # Esempio: se il load balancer avesse bisogno di meno RAM, potresti fare:
+      # memory = 1024
     }
+  }
+  # Costruisce la mappa finale delle VM iniettando i parametri hardware di default
+  vms = {
+    for vm_id, vm_config in local.vms_raw : vm_id => merge(local.vm_default, vm_config)
   }
 }
